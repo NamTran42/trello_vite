0421848b104e1caa95d26650b84165f384e56e87
Bean Tran <namtran0428@gmail.com>
2025-01-11 17:08:25 +0700

    logic frag/drop

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -7,14 +7,31 @@ import {
   MouseSensor,
   TouchSensor,
   // if U want to use pointerSensor U have to use additional css value "touch-action: 'none'" which U defined in Css-custom-dndKit
-  PointerSensor, 
+  // PointerSensor,
   useSensor,
   useSensors,
+  DragOverlay,
+  defaultDropAnimationSideEffects,
+  closestCorners,
 } from "@dnd-kit/core";
 import { arrayMove } from "@dnd-kit/sortable";
+import Column from "./ListColumns/Column/Column";
+import Card from "./ListColumns/Column/ListCards/Card/Card";
+import { cloneDeep } from "lodash";
+
+const ACTIVE_DRAG_ITEM_TYPE = {
+  COLUMN: "ACTIVE_DRAG_ITEM_TYPE_COLUMN",
+  CARD: "ACTIVE_DRAG_ITEM_TYPE_CARD",
+};
 
 function BoardContent({ board }) {
   const [orderedColumns, SetOrderedColumns] = useState([]);
+
+  // At any one time only one element can be dragged or dropped (column or card).
+  const [activeDargItemId, SetActiveDargItemId] = useState(null);
+  const [activeDargItemType, SetActiveDargItemType] = useState(null);
+  const [activeDargItemData, SetActiveDargItemData] = useState(null);
+  const [oldColumnDraggingCard, SetOldColumnDraggingCard] = useState(null);
 
   useEffect(() => {
     SetOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, "_id"));
@@ -30,24 +47,209 @@ function BoardContent({ board }) {
   // Use many points, *Recommended to use both mouse and touch sensor for smooth experience on mobile devices
   const mySensors = useSensors(mouseSensor, touchSensor);
 
-  const handleDragEnd = (event) => {
-    const { active, over } = event;
+  const findColumnByCardId = (cardId) => {
+    return orderedColumns.find((column) =>
+      column?.cards?.map((card) => card._id).includes(cardId)
+    );
+  };
 
-    if (!over) return;
-    if (active.id !== over.id) {
-      // get old index
-      const oldIndex = orderedColumns.findIndex((i) => i._id === active.id);
-      // get new index
-      const newIndex = orderedColumns.findIndex((i) => i._id === over.id);
-      // use arrayMove of dndKit to sort array of original column
-      const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex);
-      // const dndOrderedColumnsIds = dndOrderedColumns.map((i) => i._id); use to update _id_sort api
-      SetOrderedColumns(dndOrderedColumns);
+  const moveCardBetweenDiffColumns = (
+    overColumn,
+    overCardId,
+    active,
+    over,
+    activeColumn,
+    activeDraggingCardId,
+    activeDraggingCardData
+  ) => {
+    SetOrderedColumns((prevColumns) => {
+      // Find index position of OverCard in Target Column (where activeCard will be drop)
+      const overCardIndex = overColumn?.cards?.findIndex(
+        (card) => card._id === overCardId
+      );
+      // logic to calculate "new cardIndex" (over/under of overCard) from "original library code"
+      let newCardIndex;
+      const isBelowOverItem =
+        active.rect.current.translated &&
+        active.rect.current.translated.top > over.rect.top + over.rect.height;
+      const modifier = isBelowOverItem ? 1 : 0;
+
+      newCardIndex =
+        overCardIndex >= 0
+          ? overCardIndex + modifier
+          : overColumn?.cards?.length + 1;
+
+      const nextColumns = cloneDeep(prevColumns);
+      const nextActiveColumn = nextColumns.find(
+        (column) => column._id === activeColumn._id
+      );
+      const nextOverColumn = nextColumns.find(
+        (column) => column._id === overColumn._id
+      );
+
+      // Old Column
+      if (nextActiveColumn) {
+        // Delete card in Active Column(old column) when you drag card from old column to other column
+        nextActiveColumn.cards = nextActiveColumn.cards.filter(
+          (card) => card._id !== activeDraggingCardId
+        );
+        // Update cardOrderIds
+        nextActiveColumn.cardOderIds = nextActiveColumn.cards.map(
+          (card) => card._id
+        );
+      }
+
+      // New Column
+      if (nextOverColumn) {
+        // Check if the card being dragged already exists in overColumn, if so, delete this card
+        nextOverColumn.cards = nextOverColumn.cards.filter(
+          (card) => card._id !== activeDraggingCardId
+        );
+        // Add the card being dragged to overColumn according to the position of the new index (toSpliced return new arr, not origin arr)
+        nextOverColumn.cards = nextOverColumn.cards.toSpliced(newCardIndex, 0, {
+          ...activeDraggingCardData,
+          columnId: nextOverColumn._id,
+        });
+        // Update cardOrderIds
+        nextOverColumn.cardOderIds = nextOverColumn.cards.map(
+          (card) => card._id
+        );
+      }
+
+      return nextColumns;
+    });
+  };
+
+  const handleDragStart = (event) => {
+    SetActiveDargItemId(event?.active?.id);
+    SetActiveDargItemType(
+      event?.active?.data?.current?.columnId
+        ? ACTIVE_DRAG_ITEM_TYPE.CARD
+        : ACTIVE_DRAG_ITEM_TYPE.COLUMN
+    );
+    SetActiveDargItemData(event?.active?.data?.current);
+    // Only action when dragging card
+    if (event?.active?.data?.current?.columnId) {
+      SetOldColumnDraggingCard(findColumnByCardId(event?.active?.id));
     }
   };
 
+  // Drag process 1 element
+  const handleDragOver = (event) => {
+    if (activeDargItemType === ACTIVE_DRAG_ITEM_TYPE.COLUMN) return;
+    const { active, over } = event;
+    if (!active || !over) return;
+
+    const {
+      id: activeDraggingCardId, // activeDraggingCard: Card is dragging
+      data: { current: activeDraggingCardData },
+    } = active;
+    const { id: overCardId } = over; // overCard: card interact with over/under card is draged
+    // Find 2 columns throught CardId
+    const activeColumn = findColumnByCardId(activeDraggingCardId);
+    const overColumn = findColumnByCardId(overCardId);
+    if (!activeColumn || !overColumn) return;
+    if (activeColumn._id !== overColumn._id) {
+      moveCardBetweenDiffColumns(
+        overColumn,
+        overCardId,
+        active,
+        over,
+        activeColumn,
+        activeDraggingCardId,
+        activeDraggingCardData
+      );
+    }
+  };
+
+  const handleDragEnd = (event) => {
+    const { active, over } = event;
+    if (!active || !over) return;
+    if (activeDargItemType === ACTIVE_DRAG_ITEM_TYPE.CARD) {
+      const {
+        id: activeDraggingCardId, // activeDraggingCard: Card is dragging
+        data: { current: activeDraggingCardData },
+      } = active;
+      const { id: overCardId } = over; // overCard: card interact with over/under card is draged
+      // Find 2 columns throught CardId
+      const activeColumn = findColumnByCardId(activeDraggingCardId);
+      const overColumn = findColumnByCardId(overCardId);
+      if (!activeColumn || !overColumn) return;
+
+      if (oldColumnDraggingCard._id !== overColumn._id) {
+        moveCardBetweenDiffColumns(
+          overColumn,
+          overCardId,
+          active,
+          over,
+          activeColumn,
+          activeDraggingCardId,
+          activeDraggingCardData
+        );
+      } else {
+        const oldCardIndex = oldColumnDraggingCard?.cards?.findIndex(
+          (i) => i._id === activeDargItemId
+        );
+        const newCardIndex = overColumn?.cards?.findIndex(
+          (i) => i._id === overCardId
+        );
+        const dndOrderedCards = arrayMove(
+          oldColumnDraggingCard?.cards,
+          oldCardIndex,
+          newCardIndex
+        );
+        SetOrderedColumns((prevColumns) => {
+          const nextColumns = cloneDeep(prevColumns);
+          const targetColumn = nextColumns.find(
+            (i) => i._id === overColumn._id
+          );
+          targetColumn.cards = dndOrderedCards;
+          targetColumn.cardOrderIds = dndOrderedCards.map((card) => card._id);
+          return nextColumns;
+        });
+      }
+    }
+
+    if (activeDargItemType === ACTIVE_DRAG_ITEM_TYPE.COLUMN) {
+      if (active.id !== over.id) {
+        // get old index
+        const oldColumnIndex = orderedColumns.findIndex(
+          (i) => i._id === active.id
+        );
+        // get new index
+        const newColumnIndex = orderedColumns.findIndex(
+          (i) => i._id === over.id
+        );
+        // use arrayMove of dndKit to sort array of original column
+        const dndOrderedColumns = arrayMove(
+          orderedColumns,
+          oldColumnIndex,
+          newColumnIndex
+        );
+        // const dndOrderedColumnsIds = dndOrderedColumns.map((i) => i._id); use to update _id_sort api
+        SetOrderedColumns(dndOrderedColumns);
+      }
+    }
+    SetActiveDargItemId(null);
+    SetActiveDargItemType(null);
+    SetActiveDargItemData(null);
+    SetOldColumnDraggingCard(null);
+  };
+
+  const dropAnimation = {
+    sideEffects: defaultDropAnimationSideEffects({
+      styles: { active: { opacity: "0.5" } },
+    }),
+  };
+
   return (
-    <DndContext onDragEnd={handleDragEnd} sensors={mySensors}>
+    <DndContext
+      sensors={mySensors}
+      collisionDetection={closestCorners}
+      onDragStart={handleDragStart}
+      onDragOver={handleDragOver}
+      onDragEnd={handleDragEnd}
+    >
       <Box
         maxWidth
         sx={{
@@ -58,6 +260,15 @@ function BoardContent({ board }) {
         }}
       >
         <ListColumns columns={orderedColumns} />
+        <DragOverlay dropAnimation={dropAnimation}>
+          {!activeDargItemType && null}
+          {activeDargItemType === ACTIVE_DRAG_ITEM_TYPE.COLUMN && (
+            <Column column={activeDargItemData} />
+          )}
+          {activeDargItemType === ACTIVE_DRAG_ITEM_TYPE.CARD && (
+            <Card card={activeDargItemData} />
+          )}
+        </DragOverlay>
       </Box>
     </DndContext>
   );
--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -10,8 +10,27 @@ import CardMedia from "@mui/material/CardMedia";
 import GroupIcon from "@mui/icons-material/Group";
 import CommentIcon from "@mui/icons-material/Comment";
 import AttachFileIcon from "@mui/icons-material/AttachFile";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 function Card({ card }) {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({ id: card._id, data: { ...card } });
+
+  const dndKitCardStyles = {
+    // If you use Transfrom like docs then get error stretch(UI) => use Translate(keep origin element, not scale)
+    // touchAction:'none' //use sendor default of PointerSensor
+    transform: CSS.Translate.toString(transform),
+    transition,
+    opacity: isDragging ? 0.5 : undefined,
+    border: isDragging ? '1px solid #2ecc71' : undefined,
+  };
   return (
     <MuiCard
       sx={{
@@ -19,6 +38,10 @@ function Card({ card }) {
         boxShadow: "0 1px 1px rgba(0, 0 ,0 ,0.2)",
         overflow: "unset",
       }}
+      ref={setNodeRef}
+      style={dndKitCardStyles}
+      {...attributes}
+      {...listeners}
     >
       {card?.cover && (
         <CardMedia sx={{ height: 140 }} image={card.cover} title={card.title} />
